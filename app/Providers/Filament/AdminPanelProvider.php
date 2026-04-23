d8cf3393afe59b0a6490b390f37e10d364ef8fe7
Besarion Archvadze <contact@acumencraft.com>
2026-04-23 08:32:49 +0400

    Change admin path to panel for testing

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -23,7 +23,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('admin')
+            ->path('panel')
             ->login()
             ->colors([
                 'primary' => Color::Amber,
