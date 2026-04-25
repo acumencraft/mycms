38bee0688da39f7081860262296a143eb463a2a9
Besarion Archvadze <contact@acumencraft.com>
2026-04-25 11:58:57 +0400

    Add Email Queue - ShouldQueue to all Mail classes

--- app/Mail/CommentReplyMail.php
+++ app/Mail/CommentReplyMail.php
@@ -2,11 +2,12 @@
 namespace App\Mail;
 use App\Models\Comment;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
-class CommentReplyMail extends Mailable
+class CommentReplyMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
     public function __construct(
--- app/Mail/NewContactAdminMail.php
+++ app/Mail/NewContactAdminMail.php
@@ -2,12 +2,13 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class NewContactAdminMail extends Mailable
+class NewContactAdminMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
